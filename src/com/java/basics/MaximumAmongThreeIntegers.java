2efebb986469e46b429fba34557ada5f6ef039b9
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-06 11:35:36 +0530

    Added : UC-1 Printing Maximum Among 3 Integers Using Genrics Method

---
+++ src/com/java/basics/MaximumAmongThreeIntegers.java
@@ -0,0 +1,37 @@
+package com.java.basics;
+
+public class MaximumAmongThreeIntegers<T>
+{
+	T num1,num2,num3;
+
+	public MaximumAmongThreeIntegers(T num1,T num2,T num3)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+	/*
+	 * Comparision of three Variables
+	 */
+	public <T extends Comparable<T>> T maxAmong3Number(T num1, T num2, T num3)
+	{
+		T max = num1;
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0 )
+			max = num1;
+		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+			max = num2;
+		else
+			max = num3;
+		return max;
+	}
+	public <T> void printMax(T num1, T num2, T num3, T max) 
+	{
+		System.out.println(max);
+	}
+	public static void main(String[] args) 
+	{ 
+		MaximumAmongThreeIntegers obj = new MaximumAmongThreeIntegers(30,20,10);
+		System.out.println("The Maximum Among 3 Integer : "+obj.maxAmong3Number(30,20,10));                 
+
+	}
+}
