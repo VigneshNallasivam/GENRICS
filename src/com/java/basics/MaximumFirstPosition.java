dd4a64b6ff284b4bcd92154512c8b3f415a57983
VigneshNallasivam <vickyshanestark@gmail.com>
2022-12-06 13:01:47 +0530

    Added : Given Max Number at 1st Position return the Same Number

---
+++ src/com/java/basics/MaximumFirstPosition.java
@@ -0,0 +1,37 @@
+package com.java.basics;
+
+public class MaximumFirstPosition<T> 
+{
+	T num1,num2,num3;
+
+	public MaximumFirstPosition(T num1,T num2,T num3)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+	/*
+	 * Comparision of three Variables
+	 */
+	public static <T extends Comparable<T>> T maxAmong3Number(T num1, T num2, T num3)
+	{
+		T max = num1;
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0 )
+			max = num1;
+		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+			max = num2;
+		else
+			max = num3;
+		return max;
+	}
+	public <T> void printMax(T num1, T num2, T num3, T max) 
+	{
+		System.out.println(max);
+	}
+	public static void main(String[] args) 
+	{ 
+		System.out.println("The Maximum Among 3 Integer : "+maxAmong3Number(40,30,20));  
+
+	}
+
+}
